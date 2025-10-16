f8ecd3afc2ce4d043354ab6a6a0abf85a7865114
ap-oliveira <ap.antonioliveira@gmail.com>
2025-10-15 22:36:13 -0300

    reorganizando

---
+++ script.js
@@ -0,0 +1,11 @@
+
+let nome = "Ana";
+console.log("multiplicação: ", nome * 2);
+
+let nome1;
+console.log("multiplicando: ", nome1 * 2);
+
+let nome2 = "Paula"
+
+let nomeToNumber = Number(nome2);
+console.log(typeof nomeToNumber, nome2);
